7882c20677a57ceee21596b383bcc13cbb689bd4
tingraffi <graffimariomartin@gmail.com>
2025-09-03 22:25:47 -0300

    Primera version con Electron

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Cambiá esta ruta si querés abrir serviciosYStock directamente
+  win.loadFile(path.join(__dirname, "clientes", "index.html"));
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
